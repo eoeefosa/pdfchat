c95b9b6a1256d6626946d3a3097656cfc94b6fe2
eoeefosa <eoeefosa@gmail.com>
2025-01-10 16:26:14 +0100

    updated code

--- src/app/components/chat.tsx
+++ src/app/components/chat.tsx
@@ -4,10 +4,25 @@ import { Input } from "@/components/ui/input";
 import { Message } from "ai/react";
 
 export function Chat() {
-  const message: Message[] = [
+  const messages: Message[] = [
     { role: "assistant", content: "hey I am your AI", id: "1" },
     { role: "user", content: "hey I am the user", id: "2" },
   ];
 
   const sources = ["I am source one", "I am source two"];
+
+  return (
+    <div className="rounded-2xl border h-[75vh] flex flex-col justify-between">
+      <div>
+        {messages.map(({ id, role, content }: Message, index) => (
+          <ChatBubble
+            key={id}
+            role={role}
+            content={content}
+            sources={role !== "assistant" ? [] : sources}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Chat } from "./components/chat";
 import { DarkModeToggle } from "./components/dark-mode-toggle";
 
 export default function Home() {
@@ -6,6 +7,13 @@ export default function Home() {
       <div className="p-4 flex h-14 item-center justify-between supports-backdrop-blur:bg-background">
         <span className="font-bold">Pdf-chat-ai-sdk</span>
         <DarkModeToggle />
+      </div>
+      <div>
+        <div className="flex flex-1 py-4">
+          <div className="w-full">
+            <Chat />
+          </div>
+        </div>
       </div>
     </main>
   );
